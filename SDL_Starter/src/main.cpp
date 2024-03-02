d5c1abbe14e5860a9a19f3e1d65b883c078b07a6
dsomorov <dsomorov@gmail.com>
2024-03-02 15:43:58 -0600

    frame rate cap

--- SDL_Starter/src/main.cpp
+++ SDL_Starter/src/main.cpp
@@ -1,9 +1,12 @@
 #include "Game.h"
 #include "SDL.h"
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
 int main(int argc, char* argv[])
 {
-
+	Uint32 frameStart, frameTime;
 	Game* game = Game::Instance();
 
 	if (game->init(
@@ -15,11 +18,16 @@ int main(int argc, char* argv[])
 	{
 		while (game->running())
 		{
+			frameStart = SDL_GetTicks();
 			game->handleEvents();
 			game->update();
 			game->render();
 
-			SDL_Delay(10); // temporary delay until framerate cap
+			frameTime = SDL_GetTicks() - frameStart;
+			if (frameTime < DELAY_TIME)
+			{
+				SDL_Delay((int)(DELAY_TIME - frameTime));
+			}
 		}
 	}
 	else
